1439f9cac27bd0822c4f60f709a94c942e8b8cd5
Pau ruizz <pau.josep.ruiz@estudiantat.upc.edu>
2021-05-17 12:32:46 +0200

    Update movements player script

--- OverCoocked/Assets/Scripts/MoveChef.cs
+++ OverCoocked/Assets/Scripts/MoveChef.cs
@@ -27,57 +27,16 @@ public class MoveChef : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
-        inputVector = new Vector3(horizontalInput * speed, rb.velocity.y, verticalInput * speed);
-        transform.LookAt(transform.position + new Vector3(inputVector.x, 0, inputVector.z));
-         
+        if (can_move)
+        {
+            inputVector = new Vector3(horizontalInput * speed, rb.velocity.y, verticalInput * speed);
+            transform.LookAt(transform.position + new Vector3(inputVector.x, 0, inputVector.z));
+        }
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        //if (can_move) MovementChef(); 
         rb.velocity = inputVector;
-
-    }
-
-    private void MovementChef()
-    {
-
-        /*Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
-        movementDirection.Normalize();
-
-        gameObject.transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
-
-        if (movementDirection != Vector3.zero)
-        {
-            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
-
-            gameObject.transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-        }*/
-      
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-
-        GameObject objeto1 = GameObject.Find("Player");
-        if (collision.gameObject.name != "Ground" && !collision.gameObject.name.Contains("tomato") )
-        {
-           
-
-           // speed = 1;
-        }
-    }
-
-    void OnCollisionExit(Collision collision)
-    {
-
-        GameObject objeto1 = GameObject.Find("Player");
-        if (collision.gameObject.name != "Ground" && !collision.gameObject.name.Contains("tomato"))
-        {
-           
-            //speed = 7; 
-        }
     }
 
 }
